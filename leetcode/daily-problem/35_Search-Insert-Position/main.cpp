8d972e06888df5b165aff9d8e29aafe6ebf2bf26
filipe.goncalves <filipe.goncalves@fabamaq.com>
2021-12-29 15:38:17 +0000

    [LeetCode][35SearchInsertPosition][UPDATE]

--- leetcode/daily-problem/35_Search-Insert-Position/main.cpp
+++ leetcode/daily-problem/35_Search-Insert-Position/main.cpp
@@ -10,18 +10,21 @@ public:
     return searchInsert( nums, target );
   }
 
+  //----------------------------------------------------------- Challenge Solution
   int searchInsert( std::vector<int>& nums, int target) {
     if( nums[0] >= target )
       return 0;
+    if( target > nums.back() )
+      return nums.size();
 
     for( size_t i = 0; i < nums.size()-1; i++){
-      std::cout << i << " - "<< nums.at(i) << std::endl;
       if( nums.at(i) < target && nums.at(i+1) >= target ){
         return i+1 ;
       }
     }
     return nums.size() ;
   }
+  //------------------------------------------------------------------------------
 };
 
 
@@ -29,6 +32,12 @@ int main(int argc, char const *argv[])
 {
   Solution mySolution;
   std::cout << mySolution.searchInsert( {1,3,5,6} , 2 ) << std::endl;
+  std::cout << mySolution.searchInsert( {1,3,5,6} , 7 ) << std::endl;
+  std::cout << mySolution.searchInsert( {1,3,5,6} , 6 ) << std::endl;
+  std::cout << mySolution.searchInsert( {1,3,5,6} , 3 ) << std::endl;
+  std::cout << mySolution.searchInsert( {1,3,5,6} , -1 ) << std::endl;
+  std::cout << mySolution.searchInsert( {1,3} , 3 ) << std::endl;
+
 
   return 0;
 }
