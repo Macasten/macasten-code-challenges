ceac3df692c7c289be76447472768609ae134251
filipe.goncalves <filipe.goncalves@fabamaq.com>
2022-01-24 21:07:49 +0000

    craetion strutur

--- helper/app.js
+++ helper/app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.listen( port , () => {
-  console.log('Server is up on port ' + port);
+  console.log('Code Challenges helper is up on port ' + port);
 })
 
 console.log("Code Challenges Helper");
